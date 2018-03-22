2b610cadff6b414db7046b39706b6c81554832e2
RobinChenRichmond <guanyu.chen@richmond.edu>
2018-03-22 12:19:16 -0400

    update onRequestPermissionRequest

--- app/src/main/java/com/example/android/wifianalyzer/MainActivity.java
+++ app/src/main/java/com/example/android/wifianalyzer/MainActivity.java
@@ -36,8 +36,64 @@ public class MainActivity extends AppCompatActivity {
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, PERMISSION_REQUEST_COARSE_LOCATION);
+        } else{
+            mainWifi = (WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
+            mainWifi.startScan();
+            Log.i(TAG, "started scan with result");
+            scanList = mainWifi.getScanResults();
+            for (ScanResult scanResult : scanList) {
+                LayoutInflater layout = getLayoutInflater();
+                View v = layout.inflate(R.layout.wifi_elem,null);
+                TextView tv = (TextView) v.findViewById(R.id.elem);
+                tv.setText(scanResult.SSID + "\n\t\tlevel: " + scanResult.level + "dBm\n\t\tfreq: " + scanResult.frequency);
+                LinearLayout ll = findViewById(R.id.wifi_container);
+                ll.addView(v);
+            }
         }
 
+        /*
+        wifiListAdapter = new WifiListAdapter(ConnectToInternetActivity.this, mItems);
+        lv.setAdapter(wifiListAdapter);
+        int size = results.size();
+        HashMap<String, Integer> signalStrength = new HashMap<String, Integer>();
+        try {
+            for (int i = 0; i < size; i++) {
+                ScanResult result = results.get(i);
+                if (!result.SSID.isEmpty()) {
+                    String key = result.SSID + " "
+                            + result.capabilities;
+                    if (!signalStrength.containsKey(key)) {
+                        signalStrength.put(key, i);
+                        mItems.add(result);
+                        wifiListAdapter.notifyDataSetChanged();
+                    } else {
+                        int position = signalStrength.get(key);
+                        ScanResult updateItem = mItems.get(position);
+                        if (calculateSignalStength(wifiManager, updateItem.level) >
+                                calculateSignalStength(wifiManager, result.level)) {
+                            mItems.set(position, updateItem);
+                            wifiListAdapter.notifyDataSetChanged();
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }*/
+
+
+
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults){
+        super.onRequestPermissionsResult(requestCode,permissions,grantResults);
+        System.out.println("onRequested");
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        }, 2000);
         mainWifi = (WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
         mainWifi.startScan();
         Log.i(TAG, "started scan with result");
